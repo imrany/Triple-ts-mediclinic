d0f276d888249676193ad1b2f617b1f24bd1d089
imrany <imranmat254@gmail.com>
2025-05-05 14:19:09 +0300

    Sign in UI update

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -130,7 +130,7 @@ export default function SignIn() {
                                 <CardTitle className="text-center mt-4 text-2xl font-bold text-gray-800">
                                     Sign in
                                 </CardTitle>
-                                <CardDescription className="text-center">Welcome back</CardDescription>
+                                <CardDescription className="text-center">Welcome to Triple TS Medclinic for better healthcare</CardDescription>
                             </CardHeader>
                             <CardContent>
                                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
@@ -178,15 +178,15 @@ export default function SignIn() {
                                             <Checkbox id="remember" />
                                             <Label htmlFor="remember">Remember me</Label>
                                         </div>
-                                        <Link to="/forgot-password" className="text-sm text-blue-500">
+                                        <Link to="/forgot-password" className="text-sm text-pink-800 hover:underline">
                                             Forgot password?
                                         </Link>
                                     </div>
-                                    <Button type="submit" className="w-full bg-pink-800 hover:bg-pink-800">
+                                    <Button type="submit" className="w-full bg-pink-800 hover:bg-pink-700">
                                         {loading ? <Loader2 className="animate-spin" /> : "Sign In"}
                                     </Button>
                                 </form>
-                                <Separator className="my-4" />
+                                {/* <Separator className="my-4" />
                                 <Button variant="outline" className="w-full flex items-center justify-center gap-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5">
                                         <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
@@ -201,10 +201,10 @@ export default function SignIn() {
                                         <path fill="#1877F2" d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                                     </svg>
                                     Continue with Facebook
-                                </Button>
+                                </Button> */}
                                 <p className="text-center text-sm mt-4">
                                     Don't have an account?{" "}
-                                    <Link to="/signup" className="text-blue-500">
+                                    <Link to="/signup" className="text-pink-800 hover:underline">
                                         Sign Up
                                     </Link>
                                 </p>
