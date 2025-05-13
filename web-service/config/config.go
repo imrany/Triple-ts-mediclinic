c29ad0f17df764af63f1a4205ea8c5eeb43a900a
imrany <imranmat254@gmail.com>
2025-05-13 12:54:45 +0300

    updated config.go to also check /app/.env

--- web-service/config/config.go
+++ web-service/config/config.go
@@ -7,14 +7,20 @@ import (
     "github.com/joho/godotenv"
 )
 
-// GetVal func to get env value
+// GetVal retrieves environment variable, attempting multiple .env file locations
 func GetVal(key string) string {
-    // load .env file
+    // Try loading local .env file first
     err := godotenv.Load(".env")
     if err != nil {
-		log.Printf("Error loading .env file: %v", err)
-		return ""
-	}
+        log.Printf("Error loading .env file: %v", err)
+        
+        // If local fails, try loading from /app/.env
+        err = godotenv.Load("/app/.env")
+        if err != nil {
+            log.Printf("Error loading /app/.env file: %v", err)
+            return "" 
+        }
+    }
     // Return the value of the variable
     return os.Getenv(key)
 }
