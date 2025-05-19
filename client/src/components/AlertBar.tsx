a3d75e2cb8f6a4a91264219331de5bcf705662d0
imrany <imranmat254@gmail.com>
2025-05-19 16:39:00 +0300

    updated alert bar

--- client/src/components/AlertBar.tsx
+++ client/src/components/AlertBar.tsx
@@ -2,7 +2,21 @@ import useIsMobile from "@/hooks/useIsMobile";
 import { AlertTriangle } from "lucide-react";
 
 export default function AlertBar() {
-    const isMobile=useIsMobile()
+    const isMobile = useIsMobile()
+    const alertMessage = (
+        <>
+            {!isMobile ? (
+                <p className="text-center">
+                    System service unavailable. If you are the owner for contract renegotiation, contact our developer <a className="text-blue-500 underline" href="mailto:imranmat254@gmail.com?subject=Triple TS MediClinic Service Inquiry">here</a>
+                </p>
+            ) : (
+                <p className="text-sm text-center">
+                    Service unavailable. contact our developer <a className="text-blue-500 underline" href="mailto:imranmat254@gmail.com?subject=Triple TS MediClinic Service Inquiry">here</a>
+                </p>
+            )}
+        </>
+    )
+    
     return (
         <div className="bg-white py-1">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -10,15 +24,7 @@ export default function AlertBar() {
                     <div className="flex items-center space-x-4 text-sm">
                         <div className="hover:text-pink-200 flex items-center gap-2">
                             <AlertTriangle width={16} height={16} />
-                            {!isMobile ? (
-                                <p className="text-center">
-                                    System service unavailable. If you are the owner for contract renegotiation, contact our developer <a className="text-blue-500 underline" href="mailto:imranmat254@gmail.com?subject=Triple TS MediClinic Service Inquiry">here</a>
-                                </p>
-                            ) : (
-                                <p className="text-sm text-center">
-                                    Service unavailable. contact our developer <a className="text-blue-500 underline" href="mailto:imranmat254@gmail.com?subject=Triple TS MediClinic Service Inquiry">here</a>
-                                </p>
-                            )}
+                            {alertMessage}
                         </div>
                     </div>
                 </div>
