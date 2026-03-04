ff51a5fa0d89fda60b05edfb3895c7c2dc943a66
imrany <imranmat254@gmail.com>
2026-03-04 13:39:24 +0300

    fix: added a comma

--- web-service/main.go
+++ web-service/main.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-    "log"
-    "os"
-    "os/signal"
-    "syscall"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "web-service/database"
-    "web-service/config"
-    "web-service/internal/router"
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/logger"
-    "github.com/gofiber/fiber/v2/middleware/cors"
+	"web-service/config"
+	"web-service/database"
+	"web-service/internal/router"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
-
 
 func main() {
 	// Database connection
 	database.Connect()
-    app := fiber.New()
+	app := fiber.New()
 
-    // Middleware configuration
-    app.Use(logger.New())
-    app.Use(cors.New(cors.Config{
-        // AllowOrigins: "http://localhost:3000, https://triple-ts-mediclinic.com, https://www.triple-ts-mediclinic.com",
-        AllowOrigins: "*"
-        AllowMethods: "GET,POST,PUT,DELETE,PATCH",
-        AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-    }))
+	// Middleware configuration
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		// AllowOrigins: "http://localhost:3000, https://triple-ts-mediclinic.com, https://www.triple-ts-mediclinic.com",
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,PUT,DELETE,PATCH",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
-    // Routes
+	// Routes
 	router.SetupRoutes(app)
 
-    // Graceful shutdown handling
-    go func() {
-        port := config.GetVal("PORT")
-        if port == "" {
-            port = "8000"
-        }
-        if err := app.Listen("0.0.0.0:" + port); err != nil {
-            log.Fatalf("Server error: %v\n", err)
-        }
-    }()
+	// Graceful shutdown handling
+	go func() {
+		port := config.GetVal("PORT")
+		if port == "" {
+			port = "8000"
+		}
+		if err := app.Listen("0.0.0.0:" + port); err != nil {
+			log.Fatalf("Server error: %v\n", err)
+		}
+	}()
 
-    quit := make(chan os.Signal, 1)
-    signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-    <-quit
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
 
-    log.Println("Shutting down server...")
-    if err := app.Shutdown(); err != nil {
-        log.Fatalf("Error shutting down: %v\n", err)
-    }
+	log.Println("Shutting down server...")
+	if err := app.Shutdown(); err != nil {
+		log.Fatalf("Error shutting down: %v\n", err)
+	}
 }
